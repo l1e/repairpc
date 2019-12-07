fda4c3c23d08c6ee0b85e9ab639ce4919eba2416
Andrey <mefston19@gmail.com>
2019-12-07 11:16:04 +0200

    I add sctive marker for language button

--- src/component/menu.js
+++ src/component/menu.js
@@ -14,48 +14,77 @@ import i18n from './i18n';
 
 class MenuPrepare extends React.Component{
 
-render(){
-    const { t } = this.props;
-    const changeLanguage = lng => {
-        i18n.changeLanguage(lng);
-    };
-    return (
-        <Fragment >
-            <Container >
-                <Row>
-                    <Col>
-                        <Navbar inverse fluid collapseOnSelect expand="lg" >
-                            <Navbar.Brand >
-                                <NavLink  to='/'>
-                                    <img className="logo" src={goalImg} alt=""/>
-                                </NavLink>
-                            </Navbar.Brand>
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonRu: 'pasive',
+            buttonEn: 'active'
+        };
+    }
+    render(){
+        const { t } = this.props;
+        const changeLanguage = lng => {
+            i18n.changeLanguage(lng);
+        };
+        const changeActive = (lang)=>{
+            console.log('active lang is:'+lang);
+            if (lang==='ru'){
+                this.setState(
+                    {buttonRu: 'active',
+                    buttonEn: 'pasive'}
+                    )
+            }else{
+                this.setState(
+                    {buttonRu: 'pasive',
+                        buttonEn: 'active'}
+                )
+            }
+        };
+        return (
+            <Fragment >
+                <Container >
+                    <Row>
+                        <Col>
+                            <Navbar inverse fluid collapseOnSelect expand="lg" >
+                                <Navbar.Brand >
+                                    <NavLink  to='/'>
+                                        <img className="logo" src={goalImg} alt=""/>
+                                    </NavLink>
+                                </Navbar.Brand>
 
-                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                                <Navbar.Collapse id="responsive-navbar-nav">
 
-                                <Nav className="ml-auto">
-                                    <li className="menu-item">
-                                        <NavLink className="nav-link" activeClassName={"active"} exact={true} to="/"><Trans i18nKey="menu_main">Главная</Trans></NavLink>
-                                    </li>
-                                    <li className="menu-item">
-                                        <NavLink className="nav-link" activeClassName="active" to="/Services" ><Trans i18nKey="menu_serv">Услуги</Trans></NavLink>
-                                    </li>
-                                    <li className="menu-item">
-                                        <NavLink className="nav-link" activeClassName="active" to="/Contact" ><Trans i18nKey="menu_cont">Контакты</Trans></NavLink>
-                                    </li>
-                                    <Nav className="switch-language">
-                                        <Button  variant="light" onClick={() => changeLanguage("ru")}>ru</Button >
-                                        <Button  variant="light" onClick={() => changeLanguage("en")}>en</Button >
+                                    <Nav className="ml-auto">
+                                        <li className="menu-item">
+                                            <NavLink className="nav-link" activeClassName={"active"} exact={true} to="/"><Trans i18nKey="menu_main">Главная</Trans></NavLink>
+                                        </li>
+                                        <li className="menu-item">
+                                            <NavLink className="nav-link" activeClassName="active" to="/Services" ><Trans i18nKey="menu_serv">Услуги</Trans></NavLink>
+                                        </li>
+                                        <li className="menu-item">
+                                            <NavLink className="nav-link" activeClassName="active" to="/Contact" ><Trans i18nKey="menu_cont">Контакты </Trans> </NavLink>
+                                        </li>
+                                        <Nav className="switch-language">
+                                            <Button className={this.state.buttonRu}  variant="light" onClick={() =>
+                                            {changeLanguage("ru");
+                                                changeActive("ru");
+                                            }
+                                            }>ru</Button >
+                                            <Button className={this.state.buttonEn} variant="light" onClick={() =>
+                                            {changeLanguage("en");
+                                                changeActive("en");
+                                            }
+                                            }>en</Button >
+                                        </Nav>
                                     </Nav>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Navbar>
-                    </Col>
-                </Row>
-            </Container>
-        </Fragment>
-    )
+                                </Navbar.Collapse>
+                            </Navbar>
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        )
 }
 }
 const Menu = withTranslation()(MenuPrepare);
