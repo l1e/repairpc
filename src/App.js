632cf87d69b240f20823980d92c8d50f9f5d9830
Andrey <mefston19@gmail.com>
2019-12-18 22:55:07 +0200

    Firebase database visible in store.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './style/App.css';
 import './style/BootstrapSettings.sass';
 
 import './component/i18n';
+import { useFirebase, useFirebaseConnect, isLoaded, isEmpty } from "react-redux-firebase";
+
 
 import Header from './component/Header';
 import Footer from "./component/Footer";
@@ -12,7 +14,15 @@ import Main from './pages/Main';
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import ConverLink from './component/ConverLink';
+
+
+const todosQuery = {
+    path: "articles",
+    queryParams: ["limitToLast=5"]
+};
+
 function App() {
+    useFirebaseConnect(() => [todosQuery]);
   return (
       <Router>
           <Fragment>
