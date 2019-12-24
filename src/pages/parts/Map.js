15063e4baa252952eb21a58ca8d62908cdf63d1d
Andrey <mefston19@gmail.com>
2019-12-24 20:51:12 +0200

    Fix issue with translate on map.

--- src/pages/parts/Map.js
+++ src/pages/parts/Map.js
@@ -5,11 +5,13 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { withTranslation, Trans  } from 'react-i18next';
+
 import '../../style/Map.css'
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class Map extends Component {
+class MapPrepare extends Component {
     static defaultProps = {
         center: {
             lat: 48.450902,
@@ -23,7 +25,9 @@ class Map extends Component {
             <Container className='map'>
                 <Row >
                     <Col lg='12'>
-                        <h2 className='section-title'>Мы на карте</h2>
+                        <h2 className='section-title'>
+                            <Trans i18nKey="map_title">Мы на карте</Trans>
+                        </h2>
                         <div style={{ height: '60vh', width: '100%' }}>
                         <GoogleMapReact
                             bootstrapURLKeys={{ key:'AIzaSyAhEinEuiO3IXu7YxlgShb1fv9I47WA3Y8' }}
@@ -43,5 +47,7 @@ class Map extends Component {
         );
     }
 }
+
+const Map = withTranslation()(MapPrepare);
 
 export default Map;
