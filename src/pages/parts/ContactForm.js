94a6941b222b6736c0cf3340b28def9c179e0e6d
Andrey <mefston19@gmail.com>
2020-01-13 21:52:44 +0200

    Complete BEM for contact section.

--- src/pages/parts/ContactForm.js
+++ src/pages/parts/ContactForm.js
@@ -22,52 +22,54 @@ function ContactFormPrepare (props){
         const { t } = props;
         return(
             <Fragment>
-                <div className={"section-contact section"+ (props.isheader==='true' ? ' page' : ' part')}>
+                <section className={"contact section"+ (props.isheader==='true' ? ' page' : ' part')}>
 
                     <Title isheader={props.isheader} titlei18nkey={props.titlei18nkey} titlei18text={props.titlei18text}/>
-                    <Container >
-                        <Row >
-                            <Col lg='6'>
-                                <div className="contact-left">
-                                    <img className='contact-left__img' src={imgContact} alt=""/>
-                                </div>
-                            </Col>
+                    <div className="contact__container-inner">
+                        <Container >
+                            <Row >
+                                <Col lg='6'>
+                                    <div className="contact__image-inner">
+                                        <img className='contact__img' src={imgContact} alt=""/>
+                                    </div>
+                                </Col>
 
-                            <Col lg='6'>
-                                <div className="contact">
-                                    <p className="contact__title">
-                                        <Trans i18nKey="contact_desc">We perform a full range of services in the field of smartphone repair in Kiev. We carry out a detailed diagnosis of the device, identify the failure and its degree, and advise the client.</Trans>
-                                    </p>
+                                <Col lg='6'>
+                                    <div className="contact__inputs-inner">
+                                        <p className="contact__title">
+                                            <Trans i18nKey="contact_desc">We perform a full range of services in the field of smartphone repair in Kiev. We carry out a detailed diagnosis of the device, identify the failure and its degree, and advise the client.</Trans>
+                                        </p>
 
-                                    <Form onSubmit={handleSubmit(onSubmit)} className='contact-form'>
-                                        <Form.Group controlId="formGroupEmail">
-                                            <Form.Label><Trans i18nKey="contact_label_email">Почта</Trans></Form.Label>
-                                            <Form.Control name="email" ref={register({ required: true, minLength: 5, maxLength: 30 })} type="email" placeholder={t('contact_placeholder_email')} />
-                                            <p className="error">
-                                                {errors.email && errors.email.types.required && (<Trans i18nKey="contact_error_email_required">Почтовый адрес является обязательным</Trans>)}
-                                                {errors.email && errors.email.types.minLength && (<Trans i18nKey="contact_error_email_minlenght">Минимальное количество символов 5</Trans>)}
-                                                {errors.email && errors.email.types.maxLength && (<Trans i18nKey="contact_error_email_maxlenght">Максимальное количество символов 30</Trans>)}
+                                        <Form onSubmit={handleSubmit(onSubmit)} className='contact__form contact-form'>
+                                            <Form.Group controlId="formGroupEmail">
+                                                <Form.Label><Trans i18nKey="contact_label_email">Почта</Trans></Form.Label>
+                                                <Form.Control name="email" ref={register({ required: true, minLength: 5, maxLength: 30 })} type="email" placeholder={t('contact_placeholder_email')} />
+                                                <p className="contact-form__error">
+                                                    {errors.email && errors.email.types.required && (<Trans i18nKey="contact_error_email_required">Почтовый адрес является обязательным</Trans>)}
+                                                    {errors.email && errors.email.types.minLength && (<Trans i18nKey="contact_error_email_minlenght">Минимальное количество символов 5</Trans>)}
+                                                    {errors.email && errors.email.types.maxLength && (<Trans i18nKey="contact_error_email_maxlenght">Максимальное количество символов 30</Trans>)}
+                                                    </p>
+                                            </Form.Group>
+                                            <Form.Group controlId="formGroupPassword">
+                                                <Form.Label><Trans i18nKey="contact_label_phone">Номер телефона</Trans></Form.Label>
+                                                <Form.Control name="phone" ref={register({ required: true, minLength: 10, maxLength: 13 })} type="tel"  placeholder={t('contact_placeholder_phone')} />
+                                                <p className="contact-form__error">
+                                                    {errors.phone && errors.phone.types.required && (<Trans i18nKey="contact_error_phone_required">Почтовый адрес является обязательным</Trans>)}
+                                                    {errors.phone && errors.phone.types.minLength && (<Trans i18nKey="contact_error_phone_minlenght">Минимальное количество символов 10</Trans>)}
+                                                    {errors.phone && errors.phone.types.maxLength && (<Trans i18nKey="contact_error_phone_maxlenght">Максимальное количество символов 13</Trans>)}
                                                 </p>
-                                        </Form.Group>
-                                        <Form.Group controlId="formGroupPassword">
-                                            <Form.Label><Trans i18nKey="contact_label_phone">Номер телефона</Trans></Form.Label>
-                                            <Form.Control name="phone" ref={register({ required: true, minLength: 10, maxLength: 13 })} type="tel"  placeholder={t('contact_placeholder_phone')} />
-                                            <p className="error">
-                                                {errors.phone && errors.phone.types.required && (<Trans i18nKey="contact_error_phone_required">Почтовый адрес является обязательным</Trans>)}
-                                                {errors.phone && errors.phone.types.minLength && (<Trans i18nKey="contact_error_phone_minlenght">Минимальное количество символов 10</Trans>)}
-                                                {errors.phone && errors.phone.types.maxLength && (<Trans i18nKey="contact_error_phone_maxlenght">Максимальное количество символов 13</Trans>)}
-                                            </p>
-                                        </Form.Group>
-                                        <Button className='contact-form__button' variant="primary" type="submit">
-                                            <Trans i18nKey="contact_submit">Отправить</Trans>
-                                        </Button>
-                                    </Form>
+                                            </Form.Group>
+                                            <Button className='contact-form__submit' variant="primary" type="submit">
+                                                <Trans i18nKey="contact_submit">Отправить</Trans>
+                                            </Button>
+                                        </Form>
 
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </section>
             </Fragment>
         )
 }
